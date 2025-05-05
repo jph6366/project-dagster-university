8cf20023480a6dce8f9c65431ed834fe2acd4024
Jackson Hardee <42678151+jph6366@users.noreply.github.com>
2025-05-05 20:51:14 +0000

    dagster+dbt lessons 1-3

--- dagster_university/dagster_and_dbt/dagster_and_dbt/definitions.py
+++ dagster_university/dagster_and_dbt/dagster_and_dbt/definitions.py
@@ -1,8 +1,8 @@
 import dagster as dg
 
-from dagster_and_dbt.assets import metrics, requests, trips
+from dagster_and_dbt.assets import metrics, requests, trips, dbt
 from dagster_and_dbt.jobs import adhoc_request_job, trip_update_job, weekly_update_job
-from dagster_and_dbt.resources import database_resource
+from dagster_and_dbt.resources import database_resource, dbt_resource
 from dagster_and_dbt.schedules import trip_update_schedule, weekly_update_schedule
 from dagster_and_dbt.sensors import adhoc_request_sensor
 
@@ -15,6 +15,7 @@ requests_assets = dg.load_assets_from_modules(
     modules=[requests],
     group_name="requests",
 )
+dbt_analytics_assets = dg.load_assets_from_modules(modules=[dbt]) # Load the assets from the file
 
 all_jobs = [trip_update_job, weekly_update_job, adhoc_request_job]
 all_schedules = [trip_update_schedule, weekly_update_schedule]
@@ -24,6 +25,7 @@ defs = dg.Definitions(
     assets=trip_assets + metric_assets + requests_assets,
     resources={
         "database": database_resource,
+        "dbt": dbt_resource
     },
     jobs=all_jobs,
     schedules=all_schedules,
--- dagster_university/dagster_and_dbt/dagster_and_dbt/resources.py
+++ dagster_university/dagster_and_dbt/dagster_and_dbt/resources.py
@@ -3,9 +3,15 @@ import os
 import boto3
 import dagster as dg
 from dagster_duckdb import DuckDBResource
+from dagster_dbt import DbtCliResource
+from dagster_and_dbt.project import dbt_project
 
 database_resource = DuckDBResource(
     database=dg.EnvVar("DUCKDB_DATABASE"),
+)
+
+dbt_resource = DbtCliResource(
+    project_dir=dbt_project,
 )
 
 if os.getenv("DAGSTER_ENVIRONMENT") == "prod":
